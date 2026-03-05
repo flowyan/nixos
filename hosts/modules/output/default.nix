e4a3a97386fdc4dd3631eb82618ac4c42e7cfb8b
Antonín <av@flwn.dev>
2026-03-06 00:41:48 +0100

    displaylink needs to be followed by eula, disabled for now

--- hosts/modules/output/default.nix
+++ hosts/modules/output/default.nix
@@ -3,6 +3,7 @@
 
   config = {
     # enable displaylink if DP altmode is not supported
-    modules.output.displaylink.enable = !config.platform.available-features.dp-alt-mode;
+    # modules.output.displaylink.enable = !config.platform.available-features.dp-alt-mode;
+    modules.output.displaylink.enable = false; # theres some eula bullshit i have to do and dont care to do right now...
   };
 }
