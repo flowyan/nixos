7672df9cbd503486127556db53023a433fe8a4b1
Antonín <av@flwn.dev>
2026-03-06 02:36:51 +0100

    removed aseprite

--- users/ant-macnix.nix
+++ users/ant-macnix.nix
@@ -34,8 +34,6 @@
       pinentry-qt # password prompt for gpg
       qbittorrent # those who download linux isos
 
-      aseprite # sprite editor
-
        # strictly system dependent things...
       # (lib.mkIf (system == "x86_64-linux") slack) # slack suuuuucks, but useful if i need any strictly x86 stuff!
       (lib.mkIf (system == "x86_64-linux") plexamp) # i loove my plexamp, but damn x86 only!
