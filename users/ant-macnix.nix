d5da50409a79c3b9e3a2b36fab0afb82167308b1
Anthony <av@flwn.dev>
2026-03-06 04:33:24 +0100

    add vscode, jetbrains-toolbox, bun, and nodejs_20 to user packages

--- users/ant-macnix.nix
+++ users/ant-macnix.nix
@@ -34,6 +34,12 @@
       pinentry-qt # password prompt for gpg
       qbittorrent # those who download linux isos
 
+      # le coding work
+      vscode
+      jetbrains-toolbox
+      bun
+      nodejs_20
+
        # strictly system dependent things...
       # (lib.mkIf (system == "x86_64-linux") slack) # slack suuuuucks, but useful if i need any strictly x86 stuff!
       (lib.mkIf (system == "x86_64-linux") plexamp) # i loove my plexamp, but damn x86 only!
