62e561ca5cc8678fc63c0f23218c8a0e011823ee
Anthony <av@flwn.dev>
2026-03-07 05:04:58 +0100

    finished up kitty config

--- users/modules/programs/kitty.nix
+++ users/modules/programs/kitty.nix
@@ -6,9 +6,48 @@
   ...
 }: let
   cfg = config.modules.kitty;
-  # base16-kitty provides the mustache template for
-  # the color scheme
-  base16-kitty = "${inputs.base16-kitty}/templates/default-256.mustache";
+  font = config.utils.fonts.active.monospace;
+  # base16-kitty color template, inlined from
+  # https://github.com/kdrag0n/base16-kitty/blob/master/templates/default-256.mustache
+  base16-kitty-template = builtins.readFile (pkgs.writeText "base16-kitty.mustache" ''
+    background #{{base00-hex}}
+    foreground #{{base05-hex}}
+    selection_background #{{base05-hex}}
+    selection_foreground #{{base00-hex}}
+    url_color #{{base04-hex}}
+    cursor #{{base05-hex}}
+    cursor_text_color #{{base00-hex}}
+    active_border_color #{{base03-hex}}
+    inactive_border_color #{{base01-hex}}
+    active_tab_background #{{base00-hex}}
+    active_tab_foreground #{{base05-hex}}
+    inactive_tab_background #{{base01-hex}}
+    inactive_tab_foreground #{{base04-hex}}
+    tab_bar_background #{{base01-hex}}
+    wayland_titlebar_color #{{base00-hex}}
+    color0  #{{base00-hex}}
+    color1  #{{base08-hex}}
+    color2  #{{base0B-hex}}
+    color3  #{{base0A-hex}}
+    color4  #{{base0D-hex}}
+    color5  #{{base0E-hex}}
+    color6  #{{base0C-hex}}
+    color7  #{{base05-hex}}
+    color8  #{{base03-hex}}
+    color9  #{{base08-hex}}
+    color10 #{{base0B-hex}}
+    color11 #{{base0A-hex}}
+    color12 #{{base0D-hex}}
+    color13 #{{base0E-hex}}
+    color14 #{{base0C-hex}}
+    color15 #{{base07-hex}}
+    color16 #{{base09-hex}}
+    color17 #{{base0F-hex}}
+    color18 #{{base01-hex}}
+    color19 #{{base02-hex}}
+    color20 #{{base04-hex}}
+    color21 #{{base06-hex}}
+  '');
 in {
   options = {
     modules.kitty = {
@@ -20,34 +59,22 @@ in {
   config = lib.mkIf cfg.enable {
     programs.kitty = {
       enable = true;
-      # package = pkgs.kitty-themes;
-      # settings = {
-      #   font.size = config.utils.fonts.active.monospace.size;
-      #   general.import = let
-      #     themeFile = pkgs.writeTextFile {
-      #       name = "base16.toml";
-      #       text = config.utils.mustache.eval-base16 (builtins.readFile base16-kitty);
-      #     };
-      #   in [themeFile];
-      #   window = {
-      #     padding = {
-      #       x = 6;
-      #       y = 6;
-      #     };
-      #     dimensions = {
-      #       columns = 55;
-      #       lines = 17;
-      #     };
-      #   };
-      #   cursor.style.shape = "Block";
-      #   terminal.shell = {
-      #     program = "zsh";
-      #     args = [
-      #       "-c"
-      #       "fastfetch && exec zsh"
-      #     ];
-      #   };
-      # };
+      font = {
+        name = font.family;
+        size = font.size;
+        package = font.package;
+      };
+      settings = {
+        window_padding_width = 6;
+        initial_window_width = "55c";
+        initial_window_height = "17c";
+        cursor_shape = "block";
+        # image support
+        graphics_protocol = "kitty";
+        allow_remote_control = "yes";
+      };
+      shellIntegration.enableZshIntegration = true;
+      extraConfig = config.utils.mustache.eval-base16 base16-kitty-template;
     };
   };
 }
