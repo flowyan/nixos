e80779a3b2a5e2a5f5da4c97402f2b014e68c0d5
Anthony <av@flwn.dev>
2026-03-12 08:18:44 +0100

    update keyboard configuration to use apple_aluminium_iso model

--- hosts/modules/input/default.nix
+++ hosts/modules/input/default.nix
@@ -5,9 +5,12 @@
   # ];
 
   config = {
-    # configure keyboard to use colemak
+    # apple_aluminium_iso: physical keyboard is ISO (Czech MBP 14"), software layout is US.
+    # The ISO model tells XKB about the extra key between Shift and Z on ISO keyboards.
+    # fnmode is set via hid_apple (see hosts/macnix.nix).
     services.xserver = {
       xkb = {
+        model = "apple_aluminium_iso";
         layout = "us";
         # variant = "colemak";
         # options = "caps:escape";
